d46bef4ca820d47194d1803af66e280c2e5cfa0d
Seigi Takada <choji.takada@gmail.com>
2020-05-16 00:08:42 +0800

    Remove defer loading of js

--- resources/views/layouts/backend.blade.php
+++ resources/views/layouts/backend.blade.php
@@ -53,7 +53,7 @@
     </div>
 
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{ asset('js/app.js') }}"></script>
 
     <!-- Custom/Override Scripts -->
     @stack('js')
